f3eed0784d8487c36be5b09d3a6e81ad1df81b4e
Gautam Singh <gautamsingh12122003@gmail.com>
2026-01-22 22:57:18 +0530

    Feat/multiply (#1)
    * Adds multiplication function for basic math support
    
    * Adds additional test cases for multiply function
    
    * Adds additional multiplication test cases

--- 00-git-github/index.js
+++ 00-git-github/index.js
@@ -4,3 +4,11 @@ function subtract(a, b) {
     if( !(a > b) ) throw new Error("Number a should be greater than b.")
     return a - b;
 }
+
+function multiply(a, b) {
+    return a * b;
+}
+
+multiply(2, 3)
+multiply(20, 3)
+multiply(31, 32)
