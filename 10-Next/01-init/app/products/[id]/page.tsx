75d009d2216303915b193353048b26371f18958e
Gautam Singh <gautamsingh12122003@gmail.com>
2026-07-18 00:25:57 +0530

    add: learned about dynamic routes in Next

---
+++ 10-Next/01-init/app/products/[id]/page.tsx
@@ -0,0 +1,7 @@
+const ProductPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const { id } = await params;
+
+    return <div>Product Number: {id}</div>;
+};
+
+export default ProductPage;
