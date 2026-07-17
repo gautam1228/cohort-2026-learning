c2ec57688cdd4413d0823405584f582d11dfe097
Gautam Singh <gautamsingh12122003@gmail.com>
2026-07-17 23:24:28 +0530

    add: initial Nextjs project setup

---
+++ 10-Next/01-init/next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    /* config options here */
+    allowedDevOrigins: ["http://localhost:3000"], // can add more origins if needed
+};
+
+export default nextConfig;
