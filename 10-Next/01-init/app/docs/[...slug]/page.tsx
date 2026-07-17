be1e2d4767ed6550b605f226c146103fa729e87b
Gautam Singh <gautamsingh12122003@gmail.com>
2026-07-18 01:03:30 +0530

    add: learned about optional catch all routes in Nextjs

--- 10-Next/01-init/app/docs/[...slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-const DynamicDocsPage = async ({params}: {params: Promise<{slug: string[]}>}) => {
-  const {slug} = await params
-  return (
-    <div>DynamicDocsPage: {slug.join('/')}</div>
-  )
-}
-
-export default DynamicDocsPage;
---
+++ 10-Next/01-init/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,10 @@
+const DynamicDocsPage = async ({
+    params,
+}: {
+    params: Promise<{ slug: string[] }>;
+}) => {
+    const { slug } = await params;
+    return <div>DynamicDocsPage: {slug?.join("/")}</div>;
+};
+
+export default DynamicDocsPage;
